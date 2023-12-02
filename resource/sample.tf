84baca3a84706226aef8fe4beb5935cc14ac1cfb
satyareddy10 <sattireddy1997@gmail.com>
2023-12-02 11:10:47 +0530

    frontend instance create and route 53 records added

--- resource/sample.tf
+++ resource/sample.tf
@@ -1,8 +1,53 @@
-resource "aws_instance" "sample"{
+resource "aws_instance" "frontend"{
      ami           = "ami-03265a0778a880afb"
      instance_type = "t2.micro"
+     vpc_security_group_ids=["sg-0da218009af18c721"]
 
      tags = {
         Name = "frontend"
      }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z047443812PAJBPLM57X"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "backend"{
+     ami           = "ami-03265a0778a880afb"
+     instance_type = "t2.micro"
+     vpc_security_group_ids=["sg-0da218009af18c721"]
+
+     tags = {
+        Name = "backend"
+     }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = "Z047443812PAJBPLM57X"
+  name    = "backend-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_instance" "mysql"{
+     ami           = "ami-03265a0778a880afb"
+     instance_type = "t2.micro"
+     vpc_security_group_ids=["sg-0da218009af18c721"]
+
+     tags = {
+        Name = "mysql"
+     }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z047443812PAJBPLM57X"
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.backend.private_ip]
 }
